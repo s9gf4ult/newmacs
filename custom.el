b675c8ef30d1bc0b11a9877103be7b58ff0d8430
Aleksey Uimanov <s9gf4ult@gmail.com>
2019-07-20 21:43:59 +0500

    fix

--- custom.el
+++ custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(global-hl-line-mode t)
+ '(initial-buffer-choice t)
+ '(recentf-max-saved-items 1000)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -12,4 +14,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#d4d4d4" :foreground "#2c2c2c" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "Liberation Mono"))))
  '(highlight ((t (:background "gainsboro")))))
--- init.el
+++ init.el
@@ -1,8 +1,11 @@
 ;;; init.el --- init                                 -*- lexical-binding: t; -*-
 
+(setq gc-cons-threshold 1000000)
+
 (setq user-init-file (or load-file-name (buffer-file-name)))
 (setq user-emacs-directory (file-name-directory user-init-file))
 (setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
 
 (require 'package)
 (package-initialize 'noactivate)
@@ -20,36 +23,119 @@
  )
 
 (use-package
- helm-config
- :config
- (helm-mode 1))
+  helm-config
+  :config
+  (helm-mode 1)
+  )
 
 (use-package
   helm
   :bind (("M-x" . helm-M-x)
          ("M-y" . helm-show-kill-ring)
-         ("C-x b" . helm-mini)
-         ("C-x C-f" . helm-find-files))
+         ("C-x C-f" . helm-find-files)
+	 ("<f7>" . helm-mini)
+	 )
   )
 
+(use-package helm-ag
+  :bind (:map helm-command-map
+	      ("g" . helm-do-ag)))
+
 (use-package
- helm-ag
- :bind (("C-c h x" . helm-do-ag-project-root)
-        ("C-c h g" . helm-do-ag)
-        )
- :after helm
- )
+ helm-rg
+ :bind (:map helm-command-map
+             ("r" . helm-rg)))
+
+(use-package
+ helm-rg
+ :bind (:map helm-command-map
+             ("r" . helm-rg)))
+
+
+(use-package
+  helm-projectile
+  :bind (("<f8>" . helm-projectile)))
 
 (use-package
  which-key
  :config
  (which-key-mode 1))
 
+(use-package shakespeare-mode
+  :mode "\\.hamlet\\'"
+  )
+
+(use-package diff-hl
+  :config
+  (global-diff-hl-mode)
+  )
+
 (use-package
  smartparens
  :config
  (smartparens-global-mode))
 
-(use-package haskell-mode)
+(use-package haskell-mode
+  :mode (("\\.cabal\\'" . cabal-mode)
+	 ("\\.hs\\'" . haskell-mode)
+	 )
+  )
+
+(use-package haskell-compile
+  :custom
+  (haskell-compile-ignore-cabal t)
+  :after haskell-mode 
+  )
 
 (use-package yasnippet)
+
+(use-package yaml-mode)
+
+(use-package shakespeare-mode)
+
+(use-package web-mode)
+
+(use-package
+ undo-tree
+ :config
+ (global-undo-tree-mode))
+
+(use-package systemd)
+
+(use-package string-inflection)
+
+(use-package protobuf-mode
+  :mode "\\.proto\\'"
+  )
+
+(use-package projectile)
+
+(use-package move-text)
+
+(use-package json-mode
+  :mode "\\.json\\'"
+  )
+
+(use-package gitignore-mode)
+
+(use-package pg-init
+  :mode (("\\.v\\'" . coq-mode))
+  )
+
+(use-package nix-mode
+  :mode "\\.nix\\'"
+  )
+
+(use-package crux
+  :bind
+  (("C-c I" . crux-find-user-init-file))
+  )
+
+(use-package
+ org
+ :bind
+ (("C-c b" . org-switchb))
+ )
+
+(dolist (f (directory-files-recursively "~/pers/projects" "\.org$"))
+	(find-file-noselect f))
