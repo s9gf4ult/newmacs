4f88bc36799b2841e90ae368100a0724b3d8751f
Aleksey Uimanov <s9gf4ult@gmail.com>
2019-07-21 22:31:56 +0500

    fix

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(before-save-hook (quote (delete-trailing-whitespace)))
  '(blink-cursor-mode nil)
  '(global-hl-line-mode nil)
  '(initial-buffer-choice t)
