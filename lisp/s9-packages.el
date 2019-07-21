8d8ded1c503ebe12e2fa2b47a629f900b5c68b88
Aleksey Uimanov <s9gf4ult@gmail.com>
2019-07-21 13:19:11 +0500

    move to file

---
+++ lisp/s9-packages.el
@@ -0,0 +1,174 @@
+;;; s9-packages.el --- load all my packages          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  
+
+;; Author:  <razor@gazoline>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(use-package
+ neotree
+ :bind (("<f12>" . neotree-toggle))
+ )
+
+(use-package
+  ace-window
+  :bind (("<S-left>" . aw-switch-prev-window)
+	 ("<S-right>" . aw-switch-next-window)
+	 )
+  :config
+  (defun aw-swich-to-nth-window (n)
+    "Switch to nth window. Count from 1"
+    (interactive)
+    (let* ((wlist (aw-window-list))
+           (w (or
+               (nth (- idx 1) wlist)
+               (car (last wlist)))))
+      (unless (null w)
+        (aw-switch-to-window w)))))
+
+(use-package
+ magit
+
+ :bind (("C-x g" . magit)
+	)
+
+
+(use-package
+  helm-config
+  :config
+  (helm-mode 1)
+  )
+
+(use-package
+  helm
+  :bind (("M-x" . helm-M-x)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x C-f" . helm-find-files)
+	 ("<f7>" . helm-mini)
+	 )
+  )
+
+(use-package helm-ag
+  :bind (:map helm-command-map
+	      ("g" . helm-do-ag)))
+
+(use-package
+ helm-rg
+ :bind (:map helm-command-map
+             ("r" . helm-rg)))
+
+(use-package
+ helm-rg
+ :bind (:map helm-command-map
+             ("r" . helm-rg)))
+
+
+(use-package
+  helm-projectile
+  :bind (("<f8>" . helm-projectile)))
+
+(use-package
+ which-key
+ :config
+ (which-key-mode 1))
+
+(use-package shakespeare-mode
+  :mode "\\.hamlet\\'"
+  )
+
+(use-package diff-hl
+  :config
+  (global-diff-hl-mode)
+  )
+
+(use-package
+ smartparens
+ :config
+ (smartparens-global-mode))
+
+(use-package haskell-mode
+  :mode (("\\.cabal\\'" . cabal-mode)
+	 ("\\.hs\\'" . haskell-mode)
+	 )
+  )
+
+(use-package haskell-compile
+  :custom
+  (haskell-compile-ignore-cabal t)
+  :after haskell-mode
+  )
+
+(use-package yasnippet)
+
+(use-package yaml-mode)
+
+(use-package shakespeare-mode)
+
+(use-package web-mode)
+
+(use-package
+ undo-tree
+ :config
+ (global-undo-tree-mode))
+
+(use-package systemd)
+
+(use-package string-inflection)
+
+(use-package protobuf-mode
+  :mode "\\.proto\\'"
+  )
+
+(use-package projectile)
+
+(use-package move-text)
+
+(use-package json-mode
+  :mode "\\.json\\'"
+  )
+
+(use-package gitignore-mode
+  :mode "\\.gitignore\\'"
+  )
+
+(use-package pg-init
+  :mode (("\\.v\\'" . coq-mode))
+  )
+
+(use-package nix-mode
+  :mode "\\.nix\\'"
+  )
+
+(use-package crux
+  :bind
+  (("C-c I" . crux-find-user-init-file))
+  )
+
+(use-package
+ org
+ :bind
+ (("C-c b" . org-switchb))
+ )
+
+
+(provide 's9-packages)
+;;; s9-packages.el ends here
