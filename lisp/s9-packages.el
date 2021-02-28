b68bd516094eabd99ba1af7c32ec88385d765675
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-02-28 20:19:14 +0500

    fix

--- lisp/s9-packages.el
+++ lisp/s9-packages.el
@@ -220,12 +220,12 @@
 
 (use-package yaml-mode
   :mode ("\\.yaml\\'" "\\.yml\\'")
-  :hook
-  ((yaml-mode
-    . (lambda ()
-        (outline-minor-mode)
-        (define-key yaml-mode-map (kbd "TAB") 'outline-toggle-children)
-        (setq outline-regexp "^ *\\([A-Za-z0-9_-]*: *[>|]?$\\|-\\b\\)"))))
+  ;; :hook
+  ;; ((yaml-mode
+  ;;   . (lambda ()
+  ;;       (outline-minor-mode)
+  ;;       (define-key yaml-mode-map (kbd "M-TAB") 'outline-toggle-children)
+  ;;       (setq outline-regexp "^ *\\([A-Za-z0-9_-]*: *[>|]?$\\|-\\b\\)"))))
   )
 
 (use-package web-mode
