d82744ad8ba10642a84a9efa1927cddc8182ea9a
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-04-02 12:16:43 +0500

    reload dir locals

--- lisp/s9-defuns.el
+++ lisp/s9-defuns.el
@@ -1,4 +1,24 @@
 
+;;;;;;;;;;;;;;;;
+;; dir locals ;;
+;;;;;;;;;;;;;;;;
+
+(defun my-reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+
+(defun my-reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the
+current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (equal default-directory dir))
+        (my-reload-dir-locals-for-current-buffer)))))
+
 (defun uniq-lines (beg end)
   "Unique lines in region.
 Called from a program, there are two arguments:
