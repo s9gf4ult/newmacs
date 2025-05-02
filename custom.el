8f9f6cb20e8c436c45ba4fc410fc6c84f10d683e
Aleksey Uymanov <s9gf4ult@gmail.com>
2025-05-02 13:00:28 +0500

    custom

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist '((".*" concat user-emacs-directory "backups")))
  '(before-save-hook '(delete-trailing-whitespace))
  '(blink-cursor-mode nil)
  '(calendar-week-start-day 1)
@@ -14,14 +13,10 @@
  '(custom-safe-themes
    '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(display-buffer-alist
-   '(("\\*haskell-compilation\\*" display-buffer-reuse-window
-      (reusable-frames . t))
-     ("magit.*" display-buffer-pop-up-window
-      (reusable-frames . t))
-     ("\\*NeoTree\\*" ignore
-      (nil))
-     (".*" display-buffer-reuse-window
-      (reusable-frames . t))))
+   '(("\\*haskell-compilation\\*" display-buffer-reuse-window (reusable-frames . t))
+     ("magit.*" display-buffer-pop-up-window (reusable-frames . t))
+     ("\\*NeoTree\\*" ignore (nil))
+     (".*" display-buffer-reuse-window (reusable-frames . t))))
  '(display-buffer-reuse-frames t)
  '(enable-local-variables :all)
  '(fill-column 80)
@@ -35,6 +30,7 @@
  '(make-backup-files nil)
  '(mouse-yank-at-point t)
  '(nix-indent-function 'nix-indent-line)
+ '(package-selected-packages nil)
  '(recentf-max-menu-items 100)
  '(recentf-max-saved-items 10000)
  '(ring-bell-function 'ignore)
